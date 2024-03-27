72b6c5d8ab0781de26594e7fafad006dd30881fa
Amos-Bista <120589997+Amos-Bista@users.noreply.github.com>
2024-03-27 15:53:53 +0545

    constructed Page for experts section

--- src/app/(Frontend)/admin/(access)/layout.tsx
+++ src/app/(Frontend)/admin/(access)/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 
-import Footer from "@/components/Footer";
+
 import React from "react";
 import AdminNav from "@/components/adminNAV";
 
