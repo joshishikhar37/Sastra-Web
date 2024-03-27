59947271e7627e76bd9dcdc52ac43787730c6088
Amos-Bista <120589997+Amos-Bista@users.noreply.github.com>
2024-03-27 12:19:02 +0545

    Page underconstruction

---
+++ src/app/(Frontend)/admin/(access)/contact/page.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default function AdminContactPage() {
+  return (
+    <section className="bg-gradient-to-r from-gray-100/90 to- bg-sky-900 ... h-screen w-screen flex justify-center">
+      <div className="mt-36 flex-col justify-center items-center">
+       
+        <div className="flex gap-4">
+          <h1 className="text-white font-black text-5xl">UNDER</h1>
+          <h1 className="font-black text-5xl">CONSTRUCTION</h1>
+        </div>
+        <div className="mx-auto flex justify-center text-center">
+          <h1 className="font-black text-3xl text-center">
+            Site is under Construction
+          </h1>
+              </div>
+              <div className="mx-auto flex justify-center text-center">
+          <h1 className="text-white font-black text-3xl text-center">
+           Please visit later
+          </h1>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/components/adminLogIn.tsx
+++ src/components/adminLogIn.tsx
@@ -2,7 +2,6 @@
 
 import React from "react";
 
-import Link from "next/link";
 import { useState } from "react";
 import { signIn } from "next-auth/react";
 import { useRouter } from "next/navigation";
@@ -29,7 +28,7 @@ export default function AdminLogIn() {
         return;
       }
 
-      router.replace("dashboard");
+      router.replace("home");
     } catch (error) {
       console.log(error);
     }
@@ -38,11 +37,15 @@ export default function AdminLogIn() {
   return (
     <div className="flex items-center justify-center h-screen w-[100%] ">
       <div className=" relative h-[60%]  flex-col justify-center shadow-lg  p-[2%] border-t-4 bg-sky-400/10 border-blue-400 rounded-xl">
-        <h1 className=" flex justify-center text-xl font-bold my-12">Welcome Adminstrator</h1>
-        
+        <h1 className=" flex justify-center text-xl font-bold my-12">
+          Welcome Adminstrator
+        </h1>
 
         <form onSubmit={handleSubmit} className="flex flex-col gap-3 mx-4">
-          <img src="https://sastranetwork.com/storage/2021/05/sastra-logo-2048x1220.png" className="h-12 w-16 absolute top-5" />
+          <img
+            src="https://sastranetwork.com/storage/2021/05/sastra-logo-2048x1220.png"
+            className="h-12 w-16 absolute top-5"
+          />
           <input
             className="pl-[5%] py-[10%] h-12 w-96  rounded-xl "
             onChange={(e) => setEmail(e.target.value)}
@@ -55,7 +58,7 @@ export default function AdminLogIn() {
             type="password"
             placeholder="Password"
           />
-          
+
           <button className="bg-blue-500 rounded-xl text-white font-bold cursor-pointer px-6 py-4 hover:bg-blue-700">
             Login
           </button>
@@ -67,16 +70,6 @@ export default function AdminLogIn() {
               </div>
             )}
           </div>
-
-          <Link
-            className="text-sm mt-3 text-right"
-            href={"/admin/adminRegisterPage"}
-          >
-            Don't have an account?{" "}
-            <span className="underline text-blue-800 hover:scale-125 ">
-              Register
-            </span>
-          </Link>
         </form>
       </div>
     </div>
